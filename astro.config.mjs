7b93c3019da20be7a82548e75db45c6f443e0a8f
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-03 09:46:13 -0700

    test page with grid showing all 4 icons preparing to grab as imnage for splash.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,72 +1,65 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
     title: 'hyperComponents',
-		description: 'HyperScript, HTMX, Web Component Library, Tailwind CSS',
+    description: 'HyperScript, HTMX, Web Component Library, Tailwind CSS',
     social: {
       github: 'https://github.com/mmlevinson/hyperComponents'
     },
-		head: [
-			{
-				tag: 'script',
-				attrs:{
-					src: 'https://unpkg.com/hyperscript.org@0.9.12',
-					defer: true
-				} 
-			},
-			{
-				tag: 'script',
-				attrs:{
-					src: 'https://unpkg.com/htmx.org@1.9.12',
-					integrity: "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2",
-					crossorigin: "anonymous",
-					defer: true
-				} 
-			},
-		],
+    head: [{
+      tag: 'script',
+      attrs: {
+        src: 'https://unpkg.com/hyperscript.org@0.9.12',
+        defer: true
+      }
+    }, {
+      tag: 'script',
+      attrs: {
+        src: 'https://unpkg.com/htmx.org@1.9.12',
+        integrity: "sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2",
+        crossorigin: "anonymous",
+        defer: true
+      }
+    }],
     sidebar: [{
       label: 'Getting Started',
       items: [
       // Each item here is one entry in the navigation menu.
-				{
-					label: 'Background',
-					link: '/about/background/'
-				}, 
-				{
-					label: 'Why hyperComponents',
-					link: '/about/why/'
-				}, 
-				{
-					label: 'Requirements',
-					link: '/about/requirements/'
-				}, 
-				{
-					label: 'Tailwind Issues',
-					link: '/about/tailwind/'
-				}, 
-				{
-					label: 'Hyperscript',
-					link: '/about/hyperscript/'
-				}, 
-				{
-					label: 'Disclaimers',
-					link: '/about/disclaimers/'
-				},
-				{
-					label: 'Licensing',
-					link: '/about/licensing/'
-				}
-			]
+      {
+        label: 'Background',
+        link: '/about/background/'
+      }, {
+        label: 'Why hyperComponents',
+        link: '/about/why/'
+      }, {
+        label: 'Requirements',
+        link: '/about/requirements/'
+      }, {
+        label: 'Tailwind Issues',
+        link: '/about/tailwind/'
+      }, {
+        label: 'Hyperscript',
+        link: '/about/hyperscript/'
+      }, {
+        label: 'Disclaimers',
+        link: '/about/disclaimers/'
+      }, {
+        label: 'Licensing',
+        link: '/about/licensing/'
+      }]
     }, {
       label: 'Components',
       autogenerate: {
         directory: 'Components'
       }
     }]
-  }), tailwind()]
+  }), tailwind()],
+  output: "server",
+  adapter: vercel({imageService:true, devImageService: 'sharp'}),
 });
