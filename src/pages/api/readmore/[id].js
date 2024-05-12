8990c6bf93c9b9da33edabffa6f53156bbda5f04
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-12 08:04:42 -0700

    ReadMoreItem button now part of a div so the twirl-icon does not get cleared when the innterHTML is updated.

--- src/pages/api/readmore/[id].js
+++ src/pages/api/readmore/[id].js
@@ -3,12 +3,15 @@ const tw = {
 	p:'text-md font-semibold leading-7 py-3'
 }
 
+import example0 from '../../../fragments/readmore/example-0.html?raw'
 import example1 from '../../../fragments/readmore/example-1.html?raw'
 import example2 from '../../../fragments/readmore/example-2.html?raw'
 import example3 from '../../../fragments/readmore/example-3.html?raw'
 import example4 from '../../../fragments/readmore/example-4.html?raw'
 
+
 const fragments = [
+  example0,
   example1,
   example2,
   example3,
