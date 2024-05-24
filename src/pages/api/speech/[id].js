e300257e1a2e1033efff3b51b461f4873bc7d31a
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-24 12:15:57 -0700

    Speech api.

---
+++ src/pages/api/speech/[id].js
@@ -0,0 +1,30 @@
+import { twMerge } from 'tailwind-merge';
+// import {tw as formStyles} from '../../testform.astro'
+const jsonPlaceholder  = import.meta.env.JSONPLACEHOLDER_API_PATH
+// const famousSpeeches = 'https://my-json-server.typicode.com/mmlevinson/hyperComponents/'
+const famousSpeechesAPI = jsonPlaceholder + 'speech'
+
+// const tw = {...formStyles}
+
+const getMarkup = (speech) => {
+
+ return  `
+ <tr>
+	<td><input type="radio" name="id" value="${speech.id}"></td>
+	<td>${speech.speaker}</td>
+	<td>${speech.title}</td>
+	<td>${speech.date}</td>
+</tr>
+`
+
+}
+
+export const GET = async ({params, request}) => {
+	const speechId = params.id
+	console.log(`speechId`, speechId)
+	console.log(`request`, request)
+	const famousSpeech = await fetch(`${famousSpeechesAPI}/${speechId}`).then(response => response.json())
+	return new Response (getMarkup(famousSpeech), {status: 200})
+	
+	
+}
---
+++ src/pages/api/speech/index.js
@@ -0,0 +1,33 @@
+import { twMerge } from 'tailwind-merge';
+// import {tw as formStyles} from '../../testform.astro'
+const jsonPlaceholder  = import.meta.env.JSONPLACEHOLDER_API_PATH
+// const famousSpeeches = 'https://my-json-server.typicode.com/mmlevinson/hyperComponents/'
+const famousSpeechesAPI = jsonPlaceholder + 'speech'
+
+// const tw = {...formStyles}
+
+const getMarkup = (speeches) => {
+
+ const markup = speeches.map((speech)=>{
+		return  `
+		<tr>
+		<td><input type="radio" name="id" value="${speech.id}"></td>
+		<td>${speech.speaker}</td>
+		<td>${speech.title}</td>
+		<td>${speech.date}</td>
+	</tr>
+	`
+ })
+
+ return markup.join('\n')
+
+}
+
+export const GET = async ({params, request}) => {
+
+	const famousSpeeches = await fetch(`${famousSpeechesAPI}/`).then(response => response.json())
+	console.log(`famousSpeeches`, famousSpeeches)
+	return new Response (getMarkup(famousSpeeches), {status: 200})
+	
+	
+}
