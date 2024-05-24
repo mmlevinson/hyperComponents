a8b853e90b0570a82e10890c3b8ab9531bea6999
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-24 12:52:21 -0700

    Working api for loading speech data from a json file.

--- src/pages/api/speech/index.js
+++ src/pages/api/speech/index.js
@@ -1,15 +1,13 @@
 import { twMerge } from 'tailwind-merge';
-import speeches from '../../../data/speeches.json?json'
+import famousSpeeches from '../../../data/speeches.json?json'
 // import {tw as formStyles} from '../../testform.astro'
-const jsonPlaceholder  = import.meta.env.JSONPLACEHOLDER_API_PATH
-// const famousSpeeches = 'https://my-json-server.typicode.com/mmlevinson/hyperComponents/'
-const famousSpeechesAPI = jsonPlaceholder + 'speech'
+
 
 // const tw = {...formStyles}
 
-const getMarkup = (speeches) => {
+const getMarkup = () => {
 
- const markup = speeches.map((speech)=>{
+ const markup = famousSpeeches.map((speech)=>{
 		return  `
 		<tr>
 		<td><input type="radio" name="id" value="${speech.id}"></td>
@@ -25,8 +23,7 @@ const getMarkup = (speeches) => {
 }
 
 export const GET = async ({params, request}) => {
-  console.log(`speech0`, speeches[0])
-	return new Response ('success', {status: 200})
+	return new Response (getMarkup(), {status: 200})
 	
 	
 }
