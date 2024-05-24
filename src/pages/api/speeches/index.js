3b0044b0ca72a04a5e29173537a5cf141746be5d
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-24 14:49:37 -0700

    Now fetching the transcriopt on click

--- src/pages/api/speeches/index.js
+++ src/pages/api/speeches/index.js
@@ -2,19 +2,18 @@ import { twMerge } from 'tailwind-merge';
 import famousSpeeches from '../../../data/famousSpeeches.json?json'
 
 const tw = {
-	tr: 'cursor pointer hover:bg-blue-300'
-}
-
-const hs = {
-	trigger: `on click put my @index into the next <output/>`,
-	change: `on change put my value into next <output/>`
+	tr: 'cursor-pointer hover:bg-blue-300'
 }
 
 const getSpeechData = () => {
 
  const markup = famousSpeeches.map((speech)=>{
 		return  `
-		<tr index="${speech.id}" script="${hs.trigger}" class=${tw.tr}>
+		<tr index="${speech.id}" class=${tw.tr} script="
+		on click 
+		fetch /api/speeches/${speech.id}?transcript=true as html then 
+		put it into the next <output/>"
+		>	
 		<td>${speech.speaker}</td>
 		<td>${speech.title}</td>
 		<td>${speech.date}</td>
@@ -27,7 +26,6 @@ const getSpeechData = () => {
 }
 
 export const GET = async ({params, request}) => {
-	console.log(`request.search`, request)
 	return new Response (getSpeechData(), {status: 200})
 	
 	
