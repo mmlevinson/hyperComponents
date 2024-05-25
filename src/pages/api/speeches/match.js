986aa53bfd4fb17bfb932b8277b544e334fac700
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-24 23:30:38 -0700

    request.text() gives search=bbb&index=3 for some reason but is usable.

--- src/pages/api/speeches/match.js
+++ src/pages/api/speeches/match.js
@@ -1,9 +1,13 @@
 
 
-export const POST = async ({url}) => {
-	console.log(`url.searchParams`, url.searchParams)
-
-	return new Response ('OK', {status: 200})
-	
-	
+export const POST = async ({url, params, request}) => {
+  
+    // Extracting data from the request body
+    const data = await request.text();
+    console.log(`data`, data);
+		const id = url.searchParams.get('id')
+		console.log(`id`, id)
+    
+    return new Response('OK', {status: 200});
 }
+
