160a262fdb6396274b71c718101999b5df8609b7
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-25 19:46:09 -0700

    Case Insensitive working

--- src/pages/api/speeches/match.js
+++ src/pages/api/speeches/match.js
@@ -6,16 +6,45 @@ const tw = {
   mark: 'bg-yellow-200'
 }
 
-function markupSpeech(speech, searchString) {
+function markupSpeech(speech, searchString, searchCriteria) {
 	let markup = swapLineEndings(speech.transcript)
 
 	if (searchString === '') {
 		return markup
 	}
+   
+	let regex = searchString
+	let modifiers = 'g'
+
+	switch (searchCriteria) {
+		case 'Case Sensitive' :
+			modifiers = 'g'	
+			break
+		case 'Case%20Insensitive' :
+			modifiers = 'gi'
+			break
+		case 'Whole Word' :
+			regex = '\\b' + searchString + '\\b'
+			break
+		case 'Partial Word' :
+			regex = '.*' + searchString + '.*'
+			break
+		case 'Multiple Words' :
+			break
+		case 'Begins With' :
+			regex = searchString + '.*'
+			break
+		case 'Ends With' :
+			regex = '.*' + searchString
+			break
+		case 'Contains':
+			regex = searchString
+				break
+	}
 
 	// Correctly create a RegExp object with dynamic searchString
-	const searchRegex = new RegExp(searchString, 'g');
-	markup = markup.replace(searchRegex, `<mark class="${tw.mark}">${searchString}</mark>`);
+	const searchRegex = new RegExp(regex, modifiers);
+	markup = markup.replace(searchRegex, `<mark class="${tw.mark}">$&</mark>`);
 
 	return markup
 }
@@ -27,6 +56,7 @@ export const POST = async ({request}) => {
 		//data = searchString=hello&speechId=1
 		const searchString = data.split('&')[0].split('=')[1]
 		const speechId = parseInt(data.split('&')[1].split('=')[1])
+		const searchCriteria = data.split('&')[2].split('=')[1]
 		
 
 		//now get the transcript
@@ -35,6 +65,6 @@ export const POST = async ({request}) => {
 				return new Response ('Speech not found')
 		}
   
-	return new Response(markupSpeech(speech, searchString), {status: 200});
+	return new Response(markupSpeech(speech, searchString, searchCriteria), {status: 200});
 }
 
