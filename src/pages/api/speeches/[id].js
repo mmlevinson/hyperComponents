27f9a5c1e41d48286c99c152e29552868bb49e71
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2024-05-27 08:51:01 -0700

    Finished polishing code ... Ready for a hyperComponents page with example.

--- src/pages/api/speeches/[id].js
+++ src/pages/api/speeches/[id].js
@@ -1,8 +1,5 @@
 import famousSpeeches from '../../../data/famousSpeeches.json?json'
 
-// import { twMerge } from 'tailwind-merge';
-// const tw = {...formStyles}
-
 export const tw = {
 	p: 'pb-6 text-md lg:text-lg text-gray-800 dark:text-neutral-100 '
 }
@@ -14,14 +11,14 @@ const speechInfo = (speech) => {
 export function swapLineEndings(text) {
 	//add a beginning and ending <p> tag
 	let markup = `<p class="${tw.p}">` + text + '</p>'
-	//the replace all \\n with <p> tags with appropriate styling
+	//then replace all \\n with <p> tags with appropriate styling
 	markup = markup.replace(/\\n/g, `</p><p class="${tw.p}">`);
 	return markup;
 }
 
 //GET
 export async function GET({ params, url }) {
-	const speechID = +params.id
+	const speechID = parseInt(params.id)
 	const speech = famousSpeeches[speechID]
 	if (!speech) {
 	    return new Response ('Speech not found')
@@ -30,8 +27,6 @@ export async function GET({ params, url }) {
 	const searchParams = url.searchParams;
 	const transcriptOnly = searchParams.get('transcript') === 'true';
 	if (transcriptOnly) {
-		// let markup = `<p class="${tw.p}">` + speech.transcript + '</p>'
-		// markup = markup.replace(/\\n/g, `</p><p class="${tw.p}">`);
 		return new Response(swapLineEndings(speech.transcript), {status: 200})
 	}
 	else {
--- src/pages/api/speeches/match.js
+++ src/pages/api/speeches/match.js
@@ -1,8 +1,7 @@
 import famousSpeeches from '../../../data/famousSpeeches.json?json'
 import {swapLineEndings, tw as baseStyles} from './[id]'
-
-
 import {defaultBackgroundColors} from '../../../components/search/searchbox.astro'
+
 let hilightColor = defaultBackgroundColors
 
 const tw = {
@@ -16,7 +15,6 @@ function markupSpeech(speech, searchString, searchCriteria) {
    
 	let regex = '\\b' + searchString + '\\w*\\b'
 	let modifiers = 'g'
-
 
 	switch (searchCriteria) {
 		case 'case-sensitive' :
@@ -39,7 +37,8 @@ function markupSpeech(speech, searchString, searchCriteria) {
 			break
 	}
 
-	// Correctly create a RegExp object with dynamic searchString
+	/* Use regex to find matches which are then wrapped with <mark> tags including
+     a Tailwind class for the hilight color 		*/
 	const searchRegex = new RegExp(regex, modifiers)
   let markup = speech.transcript
 	markup = markup.replace(searchRegex, `<mark class="${hilightColor}">$&</mark>`);
@@ -48,19 +47,19 @@ function markupSpeech(speech, searchString, searchCriteria) {
 }
 
 export const POST = async ({request}) => {
-    // Extracting data from the request body
-    const data = await request.text()
-		console.log(`data`, data)
+    // Extract data from the request.text()
 		//data = data search=xyz&speech-index=15&search-criteria=Contains&hilight-color=bg-yellow-200
+    const data = await request.text()
+		// console.log(`data`, data)
 		const searchString = decodeURIComponent(data.split('&')[0].split('=')[1])
 		const speechId = parseInt(data.split('&')[1].split('=')[1])
 		const searchCriteria = decodeURIComponent(data.split('&')[2].split('=')[1])
-		.toLowerCase()
-		.replace(' ', '-')
+			.toLowerCase()
+			.replace(' ', '-')
 		hilightColor = data.split('&')[3].split('=')[1]
-		console.log(`searchString`, searchString)
-		console.log(`searchCriteria`, searchCriteria)
-		console.log(`hilightColor`, hilightColor)
+		// console.log(`searchString`, searchString)
+		// console.log(`searchCriteria`, searchCriteria)
+		// console.log(`hilightColor`, hilightColor)
 
 		//now get the transcript
 		const speech = famousSpeeches[speechId]
