d48edd13297a1064a4ba79bd1cc0f11473da4bac
mmlevinson <77897263+mmlevinson@users.noreply.github.com>
2026-09-13 08:18:44 -0700

    WIP trying to get the #search-container bgColor to be the selected row text color in the <table> so there is color harmony.  Not working yet, but can detect the search bgColor by polling its data-hilight-color attribute.

--- src/pages/api/speeches/index.js
+++ src/pages/api/speeches/index.js
@@ -23,8 +23,11 @@ const getSpeechData = () => {
 					put it into the #{'speech-title'}
 					fetch /api/speeches/${speech.id}?transcript=true as html then 
 					put it into the #{'speech-transcript'}
-					remove .${tw.rowHilight} from <tr/> 
-					add .${tw.rowHilight} to me
+					set rowHiliteColor to the @data-hilight-color of the #{'search-container'}
+					log rowHiliteColor
+          set textRowHiliteColor to rowHiliteColor.replace('bg-','text-')
+          log textRowHiliteColor
+					add .{textRowHiliteColor} to me 
 					go to the  #{'searchbox'} smoothly then settle then 
 					send reset to the #{'searchbox'} then
 					set the value of the #{'speech-index'} to the ${speech.id}
